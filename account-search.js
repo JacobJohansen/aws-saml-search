5661d4465164e07b68f18df48f69e5016f094c17
Scott Peshak <scott.peshak@meredith.com>
2019-07-23 15:10:32 -0500

    Load javascript from remote URLs

--- account-search.js
+++ account-search.js
@@ -39,7 +39,14 @@ function accountSearch(qry, callback) {
 
 
 function decoratePage() {
-  $('head').append('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">');
+  $('head').append(`
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  `);
 
   $('#saml_form').prepend('<br><p style="font-size: 16px; padding-left: 20px;">Search for a role:</p><div class="saml-account"><input type="text" id="account-search" class="form-control basicAutoComplete" autocomplete="off" placeholder="Type account alias, id, or role..."></div>');
 
